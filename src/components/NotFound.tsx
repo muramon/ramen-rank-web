ff6dfeb73a68a09e770f047c8defdfdb38195eda
muramon <naoki.muramoto@gmail.com>
2024-03-24 14:06:27 +0900

    add not found page

---
+++ src/components/NotFound.tsx
@@ -0,0 +1,16 @@
+import Appbar from './Appbar';
+
+
+export const NotFound = () => {
+
+    return (
+        <>
+        <Appbar/>
+        <div className="main-content">
+            <h1>お探しのページは見つかりませんでした。</h1>
+        </div>
+        </>
+        );
+}
+
+export default NotFound
